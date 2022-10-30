46de0242d96c05836925571ce302509f0a86edcc
GuilleX7 <guillermox7@gmail.com>
2022-10-30 20:10:39 +0100

    Add webpack & babel to the build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports.default = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    library: {
+      type: 'umd',
+      name: 'jasm',
+    },
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+};
